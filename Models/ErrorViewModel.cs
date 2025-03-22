e997e36e21ff702f69e197b56e26f0d5d4b5d178
Mordahaoff <artemiymorev2016@gmail.com>
2025-03-22 16:40:37 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Malyshev_Project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
