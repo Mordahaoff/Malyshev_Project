9fdcdf7b59931f05d1ee4afdda0bba51c6faf914
Mordahaoff <artemiymorev2016@gmail.com>
2025-04-18 13:43:00 +0300

    FIX ProductCatalog.cs: switch

--- Models/ProductCatalog.cs
+++ Models/ProductCatalog.cs
@@ -9,9 +9,9 @@
 		public string Units = product.CategoryId switch
 		{
 			1 => "шт. по 40 г.",
-			< 5 => "г.",
+			<= 5 => "г.",
 			6 => "мл.",
-			_ => throw new Exception("ProductCatalogException")
+			_ => throw new Exception("ProductCatalogException: Get out of range a switch.")
 		};
 	}
 }
--- obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e3c37ff4aa42ebe15898ae7030235768fa9b3a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0301bba8bbe68eca4429437c3ec9c76d3be3fe85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
