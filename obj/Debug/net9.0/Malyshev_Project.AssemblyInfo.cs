e3258c0e9b8f182e7f15066dc44849c6def602eb
Юлия Баканова <yulbakanova@gmail.com>
2025-05-04 15:57:45 +0300

    Исправление элементов, добавление кнопок в личном кабинете

--- obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7d549d16efd57d689de54d9fe5efb8bedce1f4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5910c6f1553cbc165f00f854a9163fabdfa0fc23")]
 [assembly: System.Reflection.AssemblyProductAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
