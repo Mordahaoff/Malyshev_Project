8cec201fd82972e637e2b12f7ade96498f68d1dc
Юлия Баканова <yulbakanova@gmail.com>
2025-05-15 20:56:34 +0300

    Исправлена пустая корзина пользователя, в личном кабинете для админситратора добавлена кнопка для перехода на админ-панель

--- obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a24884b1ed73a10dfe2cdfe33931cbb210ec6aa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c427da5518878c3ce59d24ac3d5ee880aaf08ec5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
