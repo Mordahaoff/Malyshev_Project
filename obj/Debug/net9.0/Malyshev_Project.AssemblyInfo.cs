a106a192403809d6fcbfda5d79ce4adff02d91f4
Юлия Баканова <yulbakanova@gmail.com>
2025-05-16 11:48:15 +0300

    Исправления
    - "О нас" - исправлены фотографии
    - "Главная" -  при наведении увеличивается кнопка "Посмотреть каталог"
    - "Личный кабинет" - обводка вокруг заказа

--- obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c427da5518878c3ce59d24ac3d5ee880aaf08ec5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cec201fd82972e637e2b12f7ade96498f68d1dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
