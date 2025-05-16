28fd85f5098285dd9c990980292057531d55b3ae
Mordahaoff <artemiymorev2016@gmail.com>
2025-05-16 17:49:02 +0300

    yes

--- obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc2d931b3f32ab71231f4cd9f1d628454efdf763")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0301052d3288e52b92b88dc3db88501bad9bf6bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
