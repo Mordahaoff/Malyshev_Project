08ba800f5555afd06da4dee5f3b3de4779b2cb4b
Юлия Баканова <yulbakanova@gmail.com>
2025-05-27 14:36:48 +0300

    Финальные доработки

--- obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Malyshev_Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3456e4b74071fafb4ac60cbb9c81775e5a0974fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b9ca24905ef7e5131d7657d7e9fb456956f916e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Malyshev_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
